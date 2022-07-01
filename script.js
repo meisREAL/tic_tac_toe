040fa5e9e9436ae953d3abffc08bda601e168670
meisReal <simas.cesnakauskas@gmail.com>
2022-07-01 09:58:01 +0300

    general idea for game objects

---
+++ script.js
@@ -0,0 +1,25 @@
+// gameboard object with module
+const Gameboard = (() => {
+    let gameboard = [];
+    let checkCell = () => {
+        //checks cell before marking it
+    };
+    let markCell = () => {
+        //code for marking cell
+    };
+})();
+
+const Player = () => {
+    const chooseCell = () => {
+        //player chooses a cell
+    };
+
+}
+
+const gameFlow = (() => {
+    let turn;
+    let winner;
+    let endGame = () => {
+        //checks if game is over or not
+    };
+})();
